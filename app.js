6fb3bdb8f67492c84bedfedbb669812514054223
Jeremy Katras <jakatras@gmail.com>
2016-09-21 15:39:00 -0400

    basic structure

---
+++ app.js
@@ -0,0 +1,7 @@
+var currentTime = document.getElementsByClassName('current-time')[0];
+var breakLength = document.getElementsByClassName('break-length')[0];
+var sessionLength = document.getElementsByClassName('session-length')[0];
+
+currentTime;
+breakLength.innerHTML = '5';
+sessionLength.innerHTML = '25';
